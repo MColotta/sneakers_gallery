7f3d0fa80e757f79f6c954fb0fd580ac70987b27
ProgramaciónWeb <ProgramacinWeb@Aula21-PC8>
2019-12-03 18:40:44 -0300

    Modificado el menú de edición de producto. Agregada libreria de font awesome

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 <title>Sneaker Shop</title>
 <link rel="stylesheet" href="css/main.css" />
 <link rel="stylesheet" href="css/arrow.css" />
+<link rel="stylesheet" href="css/bootstrap.min.css" />
 <link
 	href="https://fonts.googleapis.com/css?family=Oswald|Quattrocento&display=swap"
 	rel="stylesheet">
@@ -12,7 +13,12 @@
 
 <body>
 	<header>
-		<h1>GALERÍA DE PRODUCTO</h1>
+		<div class="nav">
+    		<h1>GALERÍA DE PRODUCTO</h1>
+    		<form action="html/form_login.html">
+        		<input type="submit" class="btn btn-primary" value="Iniciar sesión" />
+    		</form>
+		</div>
 		<hr>
 	</header>
 
@@ -22,7 +28,7 @@
 	include_once 'includes/db.php';
 	$con = openCon('config/db_products.ini');
 	$con -> set_charset("utf8mb4");
-	$sql = "SELECT s.model as modelo, s.price as precio, s.image as zapatilia, s.observation, date_format(s.initial_date, '%d-%m-%Y') as fecha, c.description as color, b.description as marca FROM sneakers s inner join brands b on s.id_brands = b.id_brand inner join colors c on s.id_colors = c.id_color order by s.initial_date;";
+	$sql = "SELECT s.free_shipping as envio, s.model as modelo, s.price as precio, s.image as zapatilia, s.observation, date_format(s.initial_date, '%d-%m-%Y') as fecha, c.description as color, b.description as marca FROM sneakers s inner join brands b on s.id_brands = b.id_brand inner join colors c on s.id_colors = c.id_color order by s.initial_date;";
 	$result = $con -> query($sql);
 	while($row = $result -> fetch_assoc()){
 	?>
@@ -31,9 +37,17 @@
         	<figure>
         		<img src="<?php echo substr($row['zapatilia'],3) ?>" alt="una zapatilia" />
         		<figcaption>
-					<h3>"<?php echo $row['marca'].' '.$row['modelo'] ?>"</h3>
-					<h3>"<?php echo $row['color'] ?>"</h3>
-					<p>"<?php echo '$ '.$row['precio'] ?>"</p>
+					<h3><?php echo $row['marca'].' '.$row['modelo'] ?></h3>
+					<h3><?php echo $row['color'] ?></h3>
+					<p class="precio"><?php echo '$ '.$row['precio'] ?></p>
+					<p class="envio">Costo del envío: 
+					<?php
+					if ($row['envio'] == 1){
+					echo "GRATIS";
+					}else{
+					echo "$ 20";
+					}
+					?></p>
 					<time>"<?php echo $row['fecha'] ?>"</time>
 				</figcaption>
         	</figure>
--- php/welcome.php
+++ php/welcome.php
@@ -2,25 +2,30 @@
 <html lang="en">
 <head>
 	<link rel= "stylesheet" href="../css/bootstrap.min.css" />
+	<link rel= "stylesheet" href="../css/menu.css" />
+	<link rel= "stylesheet" href="../css/fontawesome.css" />
+	<link rel= "stylesheet" href="../css/all.css" />
 	<meta charset="UTF-8" />
 	<title>Document</title>
 </head>
 
 <body>
-</body>
-</html>
-
 <?php
 session_start();
 if ($_SESSION['logueado']) {
-
     echo "BIENVENID@, " .$_SESSION['uname']."<br/>";
     echo "Hora de conexión: " .$_SESSION['time']."<br/>";
     echo "<a href=' Logout.php'> Logout</a>";
-    echo "<h1 class='text-center'>Opciones de menú</h1>";
-    echo "<a href='insert_products.php' style=display:flex;justify-content:center;>Insertar productos</a>";
-    echo "<a href='list_products.php' style=display:flex;justify-content:center;>Modificar productos</a>";
-    echo "<a href='list_products.php' style=display:flex;justify-content:center;>Eliminar productos</a>";
 } else
     header("location:../form_login.html");
-?>
+?>    
+
+    <h1 class='text-center'>Opciones de menú</h1>
+    <div id="container">
+        <a href='insert_products.php' class="btn-action" style=display:flex;justify-content:center;><i class="fas fa-plus-circle fa-2x"></i>Agregar productos</a>
+        <a href='list_products.php' class="btn-action" style=display:flex;justify-content:center;><i class="fas fa-edit fa-2x"></i>Editar productos</a>
+        <a href='list_products.php' class="btn-action" style=display:flex;justify-content:center;><i class="fas fa-minus-circle fa-2x"></i>Eliminar productos</a>
+	</div>
+
+</body>
+</html>
